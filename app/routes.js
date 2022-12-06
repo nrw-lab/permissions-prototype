1474900ecbd5136fced17652668927178635db8c
Colm Britton <colmjude@gmail.com>
2022-12-06 15:27:58 +0000

    Route hunting to can't

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,14 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/en/permissions/check/are_you_charging', function(req, res) {
+    const activity = req.session.data['activity']
+    if (activity === 'hunting' || activity === 'quad-biking') {
+        res.redirect('/en/permissions/check/cant_do')
+    } else {
+        res.render('en/permissions/check/are_you_charging');
+    }
+    
+});
+
 module.exports = router
