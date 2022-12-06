69de145148e4754e356e98b83105d0d2002a646c
Colm Britton <colmjude@gmail.com>
2022-12-06 16:39:00 +0000

    route if charging to ask permission page

--- app/routes.js
+++ app/routes.js
@@ -5,10 +5,32 @@ const router = express.Router()
 
 router.get('/en/permissions/check/are_you_charging', function(req, res) {
     const activity = req.session.data['activity']
-    if (activity === 'hunting' || activity === 'quad-biking') {
-        res.redirect('/en/permissions/check/cant_do')
+
+    // don't check if need to end journey for user if coming from summary page
+    if (!req.headers.referer.includes("/check/summary")) {
+        if (activity === 'hunting' || activity === 'quad-biking') {
+            res.redirect('/en/permissions/check/cant_do')
+        } else {
+            res.render('en/permissions/check/are_you_charging');
+        }
     } else {
         res.render('en/permissions/check/are_you_charging');
+    }
+    
+});
+
+router.get('/en/permissions/check/choose_site', function(req, res) {
+    const isCharging = req.session.data['are-you-charging']
+
+    // don't check if need to end journey for user if coming from summary page
+    if (!req.headers.referer.includes("/check/summary")) {
+        if (isCharging === 'yes') {
+            res.redirect('/en/permissions/check/ask_permission')
+        } else {
+            res.render('en/permissions/check/choose_site');
+        }
+    } else {
+        res.render('en/permissions/check/choose_site');
     }
     
 });
