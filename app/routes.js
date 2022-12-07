630446d4be4b3b265c7cb2b20fb51450f3c1fae9
Colm Britton <colmjude@gmail.com>
2022-12-07 13:12:06 +0000

    Hunting to trail hunting

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ router.get('/en/permissions/check/are_you_charging', function(req, res) {
 
     // don't check if need to end journey for user if coming from summary page
     if (!req.headers.referer.includes("/check/summary")) {
-        if (activity === 'hunting' || activity === 'quad-biking') {
+        if (activity === 'trail-hunting' || activity === 'quad-biking') {
             res.redirect('/en/permissions/check/cant_do')
         } else {
             res.render('en/permissions/check/are_you_charging');
